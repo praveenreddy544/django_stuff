1e585f570a3b15848485e2075055f567c6e7f527
praveen poreddy <praveenporeddy8@gmail.com>
2020-06-17 15:25:41 -0500

    modifed settings.py with heroku inclusion

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "home_page"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
