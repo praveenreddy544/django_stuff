4d445b31c53747ecab7a3166c72cd37c3d0ed6e4
praveen poreddy <praveenporeddy8@gmail.com>
2020-06-17 15:03:44 -0500

    added requirements.txt

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -11,21 +11,30 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+env = environ.Env(SECRET_KEY = str,)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '868+u*)y#(by=brwhf8@oie39oy7n9ts_9y=u-zq^g0n1v61rs'
+#SECRET_KEY = '868+u*)y#(by=brwhf8@oie39oy7n9ts_9y=u-zq^g0n1v61rs'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
